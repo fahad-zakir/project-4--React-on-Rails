7f692058d2dde377650d3dab57e1b7818409829e
fahad-zakir <fahadzakir81@gmail.com>
2018-02-26 10:38:17 -0500

    fixed the procfiles

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Cities from './components/Cities'
-import SingleCity from './components/SingleCity'
+// import Cities from './components/Cities'
+// import SingleCity from './components/SingleCreature'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Cities} />
-            <Route path="/:id" component={SingleCity} />
-          </Switch>
-        </div>
-      </Router>
+      // <Router>
+      //   <div>
+      //     <Switch>
+      //       <Route exact path="/" component={Cities} />
+      //     </Switch>
+      //   </div>
+      // </Router>
+      <div>Hello from App.js!</div>
     )
   }
 }
