5523807f37523fde349647597c560708d1edb089
fahad-zakir <fahadzakir81@gmail.com>
2022-12-22 14:30:42 -0500

    updated-data-rails-ruby

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225230500) do
-
+ActiveRecord::Schema[7.0].define(version: 2018_02_25_230500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,8 +22,8 @@ ActiveRecord::Schema.define(version: 20180225230500) do
     t.string "age_requirement"
     t.string "admission_cost"
     t.bigint "city_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["city_id"], name: "index_activities_on_city_id"
   end
 
@@ -38,16 +37,16 @@ ActiveRecord::Schema.define(version: 20180225230500) do
     t.string "state"
     t.string "population"
     t.string "city_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
     t.bigint "user_id"
     t.bigint "activity_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["activity_id"], name: "index_comments_on_activity_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -56,8 +55,8 @@ ActiveRecord::Schema.define(version: 20180225230500) do
     t.string "name"
     t.string "city"
     t.string "user_photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   add_foreign_key "activities", "cities"
